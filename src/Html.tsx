065532bceeb1e7277768c16b9093608331659ea2
h4ntyum1 <123522477+cadenceblorbo@users.noreply.github.com>
2026-03-09 19:57:13 -0400

    change null to never

--- src/Html.tsx
+++ src/Html.tsx
@@ -86,7 +86,7 @@ export const Html = React.forwardRef(
   ) => {
     const { gl, camera, scene, size } = useThree();
     const [el] = React.useState<HTMLDivElement>(() => document.createElement('div'));
-    const root = React.useRef<ReactDOM.Root>(null);
+    const root = React.useRef<ReactDOM.Root>(null!);
     const group = React.useRef<Group>(null!);
     const oldZoom = React.useRef(0);
     const oldPosition = React.useRef([0, 0]);
